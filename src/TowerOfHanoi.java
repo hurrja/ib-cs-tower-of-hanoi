84a5c67cb862424d5c411734382f5a2869515541
Jarmo Hurri <jarmo.hurri@iki.fi>
2023-07-05 16:09:05 +0300

    * correct copyright info

--- src/TowerOfHanoi.java
+++ src/TowerOfHanoi.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2020 Jarmo Hurri
+// Copyright (C) 2020-2023 Jarmo Hurri, Aleksei Terin
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
